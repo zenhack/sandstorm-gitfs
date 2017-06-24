4fcbd25b4388ac287b3298530f322756e3f4c8f7
Ian Denhardt <ian@zenhack.net>
2017-06-23 22:00:54 -0400

    Fix and expand test.
    Same issue with scanning the hash. Also, fix a typo, and examine the
    "types.go" file.

--- gitfs_test.go
+++ gitfs_test.go
@@ -15,22 +15,25 @@ import (
 const testTree = "247d84d82e4ed81e73661febe5be5952bfd23d10"
 
 func TestRootDir(t *testing.T) {
+	ctx := context.TODO()
 	dir := &Dir{
 		TreeEntry: git.TreeEntry{
 			Mode: 0100755,
 			Type: "tree",
 		},
 		g: git.Git{
-			os.Getenv("PWD"),
+			os.Getenv("PWD") + "/.git",
 		},
 	}
-	hash := dir.Hash[:]
-	_, err := fmt.Sscanf(testTree, "%040x", &hash)
+	hSlice := make([]byte, len(dir.Hash))
+	_, err := fmt.Sscanf(testTree, "%040x", &hSlice)
 	if err != nil {
 		t.Fatal(err)
 	}
+	copy(dir.Hash[:], hSlice)
+
 	root := filesystem.Directory_ServerToClient(dir)
-	info, err := root.Stat(context.TODO(), func(p filesystem.Node_stat_Params) error {
+	info, err := root.Stat(ctx, func(p filesystem.Node_stat_Params) error {
 		return nil
 	}).Info().Struct()
 	if err != nil {
@@ -40,9 +43,30 @@ func TestRootDir(t *testing.T) {
 		t.Fatal("Git directories should be executable!")
 	}
 	if info.Writable() {
-		t.Fatal("Git objecst should be read-only!")
+		t.Fatal("Git objects should be read-only!")
 	}
 	if info.Which() != filesystem.StatInfo_Which_dir {
 		t.Fatal("Wrong type from stat()")
 	}
+
+	info, err = root.Walk(ctx, func(p filesystem.Directory_walk_Params) error {
+		p.SetName("types.go")
+		return nil
+	}).Node().Stat(ctx, func(p filesystem.Node_stat_Params) error {
+		return nil
+	}).Info().Struct()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if info.Executable() {
+		t.Fatal("types.go should not be executable")
+	}
+	if info.Writable() {
+		t.Fatal("Git objects should be read-only!")
+	}
+	if info.Which() != filesystem.StatInfo_Which_file {
+		t.Fatal("Wrong type from stat()")
+	}
+	// TODO: check size
 }
